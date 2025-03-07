d4da327e804774fdeb362b37cf0e9da713784e86
Aoda <fba.gourelgnail.comt>
2025-03-07 20:03:25 +0100

    Format code

--- icecream_api/urls.py
+++ icecream_api/urls.py
@@ -1,17 +1,19 @@
-from django.urls import include, path
+from django.urls import path
 
-from icecream_api.views.batchs import BatchListApiView, BatchRefillApiView
-from icecream_api.views.commands import (CommandDetailApiView,
-                                         CommandListApiView,
-                                         CommandSubmitApiView)
-from icecream_api.views.items import ItemDetailApiView, ItemListApiView
+from icecream_api.views.batchs import BatchsListApiView, BatchRefillApiView
+from icecream_api.views.commands import (
+    CommandDetailApiView,
+    CommandsListApiView,
+    CommandSubmitApiView,
+)
+from icecream_api.views.items import ItemDetailApiView, ItemsListApiView
 
 urlpatterns = [
-    path("items", ItemListApiView.as_view()),
+    path("items", ItemsListApiView.as_view()),
     path("item/<int:item_id>", ItemDetailApiView.as_view()),
     path("command/submit", CommandSubmitApiView.as_view()),
-    path("commands", CommandListApiView.as_view()),
+    path("commands", CommandsListApiView.as_view()),
     path("command/<str:command_id>", CommandDetailApiView.as_view()),
-    path("batchs", BatchListApiView.as_view()),
+    path("batchs", BatchsListApiView.as_view()),
     path("batch/refill/<str:batch_name>", BatchRefillApiView.as_view()),
 ]
