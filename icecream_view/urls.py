7df435b9f361c9d4a4754cf6fccde7374c75cc37
Aoda <fba.gourelgnail.comt>
2025-03-07 20:03:18 +0100

    Rename view

--- icecream_view/urls.py
+++ icecream_view/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 from icecream import settings
 from icecream_view.views.admin import admin_view
-from icecream_view.views.command import commande_view
+from icecream_view.views.make_command import make_command_view
 from icecream_view.views.retrieve_command import retrieve_command_view
 
 urlpatterns = [
-    path("", commande_view, name="command"),
+    path("", make_command_view, name="make_command"),
     path("retrieve/", retrieve_command_view, name="retrieve_command"),
     path("admin/", admin_view, name="admin"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- icecream_view/views/make_command.py
+++ icecream_view/views/make_command.py
@@ -1,22 +1,18 @@
 import json
-import os
 
 import requests
 from django.shortcuts import render
-from django.templatetags.static import static
 
-from icecream import settings
 from icecream_view import config
 
 
-def commande_view(request):
+def make_command_view(request):
 
     res = requests.get(config.API + "/items")
     flavours = json.loads(res.text)
-    path = settings.STATIC_ROOT
 
     return render(
         request,
-        "command.html",
+        "make_command.html",
         {"flavours": flavours, "api": config.API},
     )
