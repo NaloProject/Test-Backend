66c0f9958e7ec0feb88ab2b917278574143006f6
Aoda <fba.gourelgnail.comt>
2025-03-09 19:04:59 +0100

    Add butons

--- icecream_view/views/admin.py
+++ icecream_view/views/admin.py
@@ -11,6 +11,10 @@ def admin_view(request):
     res = requests.get(config.API + "/commands")
     commands = json.loads(res.text)
 
+    total_amount = 0.0
+    for command in commands:
+        total_amount += command["price"]
+
     res = requests.get(config.API + "/batchs")
     batchs = json.loads(res.text)
 
@@ -21,5 +25,6 @@ def admin_view(request):
             "batchs": batchs,
             "commands": commands,
             "api": config.API,
+            "totalAmount": total_amount,
         },
     )
